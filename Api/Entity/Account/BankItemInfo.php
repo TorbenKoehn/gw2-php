b3421744a5df88f9e089fa2237801c104fb04da0
Torben Köhn <tk@talesoft.io>
2015-11-25 19:05:44 +0100

    Fixed a bug of Bank Items showing only one entry for each item id

--- Api/Entity/Account/BankItemInfo.php
+++ Api/Entity/Account/BankItemInfo.php
@@ -7,4 +7,12 @@ use GuildWars2\Api\Entity\ItemInfoBase;
 
 class BankItemInfo extends ItemInfoBase
 {
+
+    public static function getIndexField()
+    {
+        //Notice that you can have multiple slots with the same
+        //item ID. There is no field "index", so he just appends
+        //the item to the entity array.
+        return 'index';
+    }
 }
